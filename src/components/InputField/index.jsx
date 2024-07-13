5eea43a15613e854b9e4bd3dc9d905abeeef77b5
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-07-13 17:53:17 -0400

    Feature: Multiform #1

---
+++ src/components/InputField/index.jsx
@@ -0,0 +1,25 @@
+export default function InputField( {className, id, type, label, inputSize, labelPos} ) {
+    const finalID = (() => {
+        if (id === undefined) return label;
+        else return id;
+    })();
+
+    const inputSizeClasses = (() => {
+        if (inputSize === "sm")
+            return "w-12";
+        if (inputSize === "lg")
+            return "w-96";
+        else return "w-48";
+    })();
+
+    return (
+        <div className={`flex p-2 z-0 w-3/4 mb-5 border-b border-slate-600 ${className}`}>
+            <div className={`rounded w-full ${labelPos === "side" && 'flex justify-between w-3/4'}`}>
+                <label className='block'>{label}</label>
+                <input id={finalID} name={finalID} 
+                    className={`pl-1 pr-1 text-left border border-black ${inputSizeClasses} ${inputSize === "sm" && 'text-center'}`} 
+                    type={type}/>
+            </div>         
+        </div>
+    );
+}
