68a7226baf4b22d531509ebc150f1ee017f234d9
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-07-29 00:58:43 -0400

    Update index.jsx

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,5 +1,5 @@
 export default function Tag({className, text}) {
     return (
-        <span className={`px-2 py-1 rounded-lg h-8 text-white font-bold text-sm ${className}`}>{text}</span>
+        <span className={`px-2 py-1 rounded-lg h-8 font-bold text-sm ${className}`}>{text}</span>
     )
 }
